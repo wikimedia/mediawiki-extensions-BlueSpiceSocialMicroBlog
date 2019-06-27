c53aec39ac87b57748eb75babcef81e81826359d
Markus Glaser <glaser@hallowelt.com>
2019-06-27 18:35:22 +0000

    Merge branch 'master' into 'master'
    Fix invalid license tags
    
    See merge request BlueSpice/mediawiki-extensions-BlueSpiceSocialMicroBlog!16
    
    (cherry picked from commit 87c4b55d069849f7b9df4ee9ef07f5c95bdec5e1)
    
    de0b079a Fix invalid license tags

--- resources/entities/bluespice.social.Entity.MicroBlog.js
+++ resources/entities/bluespice.social.Entity.MicroBlog.js
@@ -5,7 +5,7 @@
  * @package    BluespiceSocial
  * @subpackage BSSocialMicroBlog
  * @copyright  Copyright (C) 2017 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
  */
 
 bs.social.EntityMicroBlog = function( $el, type, data ) {
--- src/NotificationConfig/Create.php
+++ src/NotificationConfig/Create.php
@@ -23,7 +23,7 @@
  * @author     Patric Wirth <wirth@hallowelt.com>
  * @package    BlueSpiceSocial
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
  * @filesource
  */
 
