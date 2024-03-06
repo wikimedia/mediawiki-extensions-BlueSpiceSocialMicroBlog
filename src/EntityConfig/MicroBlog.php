c8ffbe401c44966526d04074c13a45919941ea72
Swapnil Srivastava <142904704+Swapnilden@users.noreply.github.com>
2024-03-06 11:52:18 +0530

    Update MicroBlog.php
    These modifications ensure that the code is well-documented, formatted correctly, and follows best practices for maintainability and readability.

--- src/EntityConfig/MicroBlog.php
+++ src/EntityConfig/MicroBlog.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * MicroBlog class for BSSocial
+ * Represents a MicroBlog configuration for the BlueSpiceSocial extension.
  *
- * add desc
+ * This class provides configuration settings and methods specific to MicroBlogs.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,121 +35,135 @@ namespace BlueSpice\Social\MicroBlog\EntityConfig;
 use BlueSpice\Social\EntityConfig\Text;
 
 /**
- * MicroBlog class for BSSocial extension
+ * Represents a MicroBlog configuration for the BlueSpiceSocial extension.
+ *
  * @package BlueSpiceSocial
  * @subpackage BSSocial
  */
 class MicroBlog extends Text {
-	/**
-	 *
-	 * @return array
-	 */
-	public function addGetterDefaults() {
-		return [];
-	}
+    /**
+     * Retrieves the default getter values.
+     *
+     * @return array An array of default getter values.
+     */
+    public function addGetterDefaults() {
+        return [];
+    }
 
-	/**
-	 *
-	 * @return string
-	 */
-	protected function get_EntityClass() {
-		return "\\BlueSpice\\Social\\MicroBlog\\Entity\\MicroBlog";
-	}
+    /**
+     * Retrieves the entity class for MicroBlog.
+     *
+     * @return string The class name of the MicroBlog entity.
+     */
+    protected function get_EntityClass() {
+        return "\\BlueSpice\\Social\\MicroBlog\\Entity\\MicroBlog";
+    }
 
-	/**
-	 *
-	 * @return array
-	 */
-	protected function get_ModuleScripts() {
-		return array_merge( parent::get_ModuleScripts(), [
-			'ext.bluespice.social.entity.text',
-			'ext.bluespice.social.entity.microblog',
-		] );
-	}
+    /**
+     * Retrieves the module scripts for MicroBlog.
+     *
+     * @return array An array of module scripts for MicroBlog.
+     */
+    protected function get_ModuleScripts() {
+        return array_merge(parent::get_ModuleScripts(), [
+            'ext.bluespice.social.entity.text',
+            'ext.bluespice.social.entity.microblog',
+        ]);
+    }
 
-	/**
-	 *
-	 * @return array
-	 */
-	protected function get_ModuleStyles() {
-		return array_merge( parent::get_ModuleStyles(), [
-			'ext.bluespice.socialmicroblog.styles'
-		] );
-	}
+    /**
+     * Retrieves the module styles for MicroBlog.
+     *
+     * @return array An array of module styles for MicroBlog.
+     */
+    protected function get_ModuleStyles() {
+        return array_merge(parent::get_ModuleStyles(), [
+            'ext.bluespice.socialmicroblog.styles'
+        ]);
+    }
 
-	/**
-	 *
-	 * @return string
-	 */
-	protected function get_TypeMessageKey() {
-		return 'bs-socialmicroblog-type';
-	}
+    /**
+     * Retrieves the message key for MicroBlog type.
+     *
+     * @return string The message key for MicroBlog type.
+     */
+    protected function get_TypeMessageKey() {
+        return 'bs-socialmicroblog-type';
+    }
 
-	/**
-	 *
-	 * @return array
-	 */
-	protected function get_Notifications() {
-		return [
-			'socialentity-microblog-create' => "\\BlueSpice\\Social\\MicroBlog\\NotificationConfig\\Create",
-		];
-	}
+    /**
+     * Retrieves the notification configurations for MicroBlog.
+     *
+     * @return array An array of notification configurations for MicroBlog.
+     */
+    protected function get_Notifications() {
+        return [
+            'socialentity-microblog-create' => "\\BlueSpice\\Social\\MicroBlog\\NotificationConfig\\Create",
+        ];
+    }
 
-	/**
-	 *
-	 * @return array
-	 */
-	protected function get_NotificationActions() {
-		return [
-			'create' => 'socialentity-microblog-create',
-		];
-	}
+    /**
+     * Retrieves the notification actions for MicroBlog.
+     *
+     * @return array An array of notification actions for MicroBlog.
+     */
+    protected function get_NotificationActions() {
+        return [
+            'create' => 'socialentity-microblog-create',
+        ];
+    }
 
-	/**
-	 *
-	 * @return true
-	 */
-	protected function get_ExtendedSearchListable() {
-		return true;
-	}
+    /**
+     * Determines if MicroBlog is listable in extended search.
+     *
+     * @return bool True if MicroBlog is listable in extended search, false otherwise.
+     */
+    protected function get_ExtendedSearchListable() {
+        return true;
+    }
 
-	/**
-	 *
-	 * @return true
-	 */
-	protected function get_EntityListSpecialTimelineTypeSelected() {
-		return true;
-	}
+    /**
+     * Determines if MicroBlog is selected in entity list special timeline type.
+     *
+     * @return bool True if MicroBlog is selected in entity list special timeline type, false otherwise.
+     */
+    protected function get_EntityListSpecialTimelineTypeSelected() {
+        return true;
+    }
 
-	/**
-	 *
-	 * @return string
-	 */
-	protected function get_CreatePermission() {
-		return 'social-microblog';
-	}
+    /**
+     * Retrieves the permission for creating MicroBlogs.
+     *
+     * @return string The permission required for creating MicroBlogs.
+     */
+    protected function get_CreatePermission() {
+        return 'social-microblog';
+    }
 
-	/**
-	 *
-	 * @return string
-	 */
-	protected function get_EditPermission() {
-		return 'social-microblog';
-	}
+    /**
+     * Retrieves the permission for editing MicroBlogs.
+     *
+     * @return string The permission required for editing MicroBlogs.
+     */
+    protected function get_EditPermission() {
+        return 'social-microblog';
+    }
 
-	/**
-	 *
-	 * @return string
-	 */
-	protected function get_DeletePermission() {
-		return 'social-microblog';
-	}
+    /**
+     * Retrieves the permission for deleting MicroBlogs.
+     *
+     * @return string The permission required for deleting MicroBlogs.
+     */
+    protected function get_DeletePermission() {
+        return 'social-microblog';
+    }
 
-	/**
-	 *
-	 * @return string
-	 */
-	protected function get_CommentPermission() {
-		return 'social-microblog-comment';
-	}
+    /**
+     * Retrieves the permission for commenting on MicroBlogs.
+     *
+     * @return string The permission required for commenting on MicroBlogs.
+     */
+    protected function get_CommentPermission() {
+        return 'social-microblog-comment';
+    }
 }
